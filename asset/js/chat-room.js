5d9156a39a1e03872fccc787933d53773f70d357
jrg4017 <jrg4017@rit.edu>
2017-02-12 05:12:12 -0500

    fix so send works

--- asset/js/chat-room.js
+++ asset/js/chat-room.js
@@ -1,27 +1,27 @@
-// <script>
-// var form = document.querySelector('form[name="chatbox"]');
-// form.addEventListener("submit", function (event) {
-//   event.preventDefault();
+// var socket = io();
+// user = Math.random();
+//
+// socket.on('chat message', function(msg) {
+//   $('#messages').append($('<li>').text(msg));
+//   console.log("hello");
+//   var m = $('#room');
+//   m.scrollTop(m.prop("scrollHeight"));
 // });
+//
+// //functions
+// function submitChat() {
+//   var msg = user + ': ' + $('#m').val();
+//   console.log(msg);
+//   socket.emit('chat message', msg );
+//   $('#m').val('');
+//   return false;
+// }
+//
+// function onEnter(event, id) {
+//   console.log('click');
+//   if (event.keyCode == 13) { document.getElementById(id).click(); }
+// }
+// function login() { console.log("hey"); }
 
-function submitChat() {
-  var msg = user + ': ' + $('#m').val();
-  socket.emit('chat message', msg );
-  $('#m').val('');
-}
 
-var socket = io();
-user = Math.random();
-
-socket.on('chat message', function(msg) {
-  $('#messages').append($('<li>').text(msg));
-  var m = $('#room');
-  m.scrollTop(m.prop("scrollHeight"));
-});
-// </script>
-
-function onEnter(event, id) {
-  if (event.keyCode == 13) { document.getElementById(id).click(); }
-}
-function login() { console.log("hey"); }
 
